e7439c79cf81b18a0339887c6b0940338eaeff57
Dawid Bialokryty <dawid.bialokryty@gmail.com>
2025-09-17 14:39:56 +0200

    Add --verbose command line argument

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
 def main():
+    if len(sys.argv) < 2:
+        sys.exit(1)
+
     print("Hello from ai-agent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    user_prompt = sys.argv[1]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents=messages
+    )
+    if "--verbose" in sys.argv:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(response.text)
 
 
 if __name__ == "__main__":
