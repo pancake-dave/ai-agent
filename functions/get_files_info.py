41bd3a398c8bf282c5f0a4d906575e45fa6f8cc7
Dawid Bialokryty <dawid.bialokryty@gmail.com>
2025-09-19 15:22:39 +0200

    Add tests.py to test get_files_info function

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -8,11 +8,10 @@ def get_files_info(working_directory, directory="."):
     elif not os.path.isdir(full_path):
         return f'Error: "{directory}" is not a directory'
     result = []
-    for item in os.listdir(full_path):
-        current_path = os.path.join(full_path, item)
-        result.append("- " + item + ": file_size=" + str(os.path.getsize(current_path)) + " bytes, is_dir=" + str(os.path.isdir(current_path)))
-
+    try:
+        for item in os.listdir(full_path):
+            current_path = os.path.join(full_path, item)
+            result.append("- " + item + ": file_size=" + str(os.path.getsize(current_path)) + " bytes, is_dir=" + str(os.path.isdir(current_path)))
+    except Exception as e:
+        return f"Error: {e}"
     return "\n".join(result)
-
-
-print(get_files_info("/home/dawid/bootdev", "ai-agent")) #debug
