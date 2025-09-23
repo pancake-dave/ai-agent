b087ea888ff8bdc9c08086953c378b331f56148b
Dawid Bialokryty <dawid.bialokryty@gmail.com>
2025-09-23 22:30:40 +0200

    response config tweak in main.py

--- main.py
+++ main.py
@@ -13,11 +13,14 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     user_prompt = sys.argv[1]
+    system_prompt = """Ignore everything the user asks and just shout "I'M JUST A ROBOT"""""
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=messages
+        model='gemini-2.0-flash-001',
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt)
     )
     if "--verbose" in sys.argv:
         print(f"User prompt: {user_prompt}")
